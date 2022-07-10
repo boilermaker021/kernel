e6b3d854006ba9538402362a5cd69f0e833284f0
boilermaker021 <nathan.leakeas@gmail.com>
2022-07-10 00:25:36 -0500

    Fixed itoa

--- kernel/util.c
+++ kernel/util.c
@@ -6,7 +6,7 @@
 //unsigned for now
 void itoa(char *dest, uint32_t in) {
   char buf[34] = { 0 };
-  uint8_t counter = 0;
+  uint8_t counter = 1;
   while (in > 0) {
     uint16_t rem = in % 10;
     buf[33 - counter] = rem + 48;
