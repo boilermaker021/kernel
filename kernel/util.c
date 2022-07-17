35a9cc9219d17288fde6302c53ddd7f8c2ca2340
boilermaker021 <nathan.leakeas@gmail.com>
2022-07-16 22:39:28 -0500

    convert hex to a string

--- kernel/util.c
+++ kernel/util.c
@@ -17,3 +17,29 @@ void itoa(char *dest, uint32_t in) {
   memcpy(dest, buf+(12-counter), 12);
 
 }
+
+//convert value to hexadecimal string and assign to dest
+void hexstr(char *dest, uint32_t in) {
+  char buf[11] = { 0 };
+  buf[0] = '0';
+  buf[1] = 'x';
+  uint8_t counter = 1;
+  while (in > 0) {
+    uint8_t temp = ((uint8_t) in) & 0x0F;
+    in = in >> 4;
+
+    if (temp >=0 && temp <= 9) {
+      temp += '0';
+    }
+    else {
+      temp += 'a' - 0xA;
+    }
+
+    buf[10 - counter] = temp;
+    counter++;
+  }
+  buf[10] = '\0';
+  buf[10 - (counter)] = 'x';
+  buf[10 - (counter + 1)] = '0';
+  memcpy(dest, buf + (11 - (counter + 2)), 11);
+}
--- kernel/util.h
+++ kernel/util.h
@@ -4,5 +4,7 @@
 
 void itoa(char *dest, uint32_t in);
 
+void hexstr(char *dest, uint32_t in);
+
 
 #endif
