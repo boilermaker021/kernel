de86db1c1418fc8a60dc055f5c58a7946d8250c6
Nathan <nathan.leakeas@gmail.com>
2023-12-19 20:42:29 -0600

    restructure directory part 2

--- kernel_entry.c
+++
@@ -1,8 +0,0 @@
-#include "include/vga.h"
-
-void kernel_main(void) {
-  struct vga_entry *term_buffer = (struct vga_entry *)VGA_MEM_START;
-  term_buffer[0].chr = 'H';
-  term_buffer[0].fg = VGA_COLOR_WHITE;
-  term_buffer[0].bg = VGA_COLOR_BLACK;
-}
