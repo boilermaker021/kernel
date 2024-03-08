b94573cb47ec28cf7c3153f5f67bc729908333db
Nathan <nathan.leakeas@gmail.com>
2024-03-07 19:36:09 -0500

    added memcpy

--- include/string.h
+++ include/string.h
@@ -4,8 +4,10 @@
 #define UPPER_CASE 1
 #define LOWER_CASE 0
 
+#include <stddef.h>
 #include <stdint.h>
 
+void *memcpy(void *dest, void *src, size_t n);
 uint32_t strlen(char *str);
 char bytetochar(uint8_t byte, uint8_t c);
 char *utoa(uint32_t integer, char *outbuf, uint8_t len, uint8_t base);
@@ -13,6 +15,5 @@ int itoa(uint32_t integer, char *outbuf, uint8_t len, uint8_t base);
 void itohexstr(unsigned int hex, char *outbuf);
 void ctohexstr(unsigned char hex, char *outbuf);
 void stohexstr(unsigned int hex, char *outbuf);
-
 
 #endif
--- src/utils/string.c
+++ src/utils/string.c
@@ -1,64 +1,73 @@
-#include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
 #include "string.h"
 
+/* dest and src must NOT overlap */
+void *memcpy(void *dest, void *src, size_t n) {
+  for (uint32_t i = 0; i < n; i++) {
+    ((char *)dest)[i] = ((char *)src)[i];
+  }
+  return dest;
+}
+
 uint32_t strlen(char *str) {
-    uint32_t size;
-    while (*str != '\0') {
-        str++;
-        size++;
-    }
-    return size;
+  uint32_t size;
+  while (*str != '\0') {
+    str++;
+    size++;
+  }
+  return size;
 }
 
 char bytetochar(uint8_t byte, uint8_t c) {
-    if (byte <=9) {
-        return '0' + byte;
-    } else {
-        return (c ? 'A' : 'a') + byte - 10;
-    }
+  if (byte <= 9) {
+    return '0' + byte;
+  } else {
+    return (c ? 'A' : 'a') + byte - 10;
+  }
 }
 
 char *utoa(uint32_t integer, char *outbuf, uint8_t len, uint8_t base) {
-    int i = 0;
-    while (integer > 0) {
-        outbuf[(len - 1) - i] = bytetochar(integer % base, UPPER_CASE); //replace
-        i++;
-        integer /= base;
-    }
-    return outbuf + len - i;
+  int i = 0;
+  while (integer > 0) {
+    outbuf[(len - 1) - i] = bytetochar(integer % base, UPPER_CASE); // replace
+    i++;
+    integer /= base;
+  }
+  return outbuf + len - i;
 }
 
 int itoa(uint32_t integer, char *outbuf, uint8_t len, uint8_t base) {
-    bool negative = integer < 0;
+  bool negative = integer < 0;
 }
 
 void itohexstr(unsigned int hex, char *outbuf) {
-    char *buf = &hex;
-    for(int i = 0; i < 4; i++) {
-        ctohexstr(buf[3-i], outbuf + i*2);
-    }
+  char *buf = &hex;
+  for (int i = 0; i < 4; i++) {
+    ctohexstr(buf[3 - i], outbuf + i * 2);
+  }
 }
 
 void stohexstr(unsigned int hex, char *outbuf) {
-    char *buf = &hex;
-    for(int i = 0; i < 2; i++) {
-        ctohexstr(buf[1-i], outbuf + i*2);
-    }
+  char *buf = &hex;
+  for (int i = 0; i < 2; i++) {
+    ctohexstr(buf[1 - i], outbuf + i * 2);
+  }
 }
 
-
 void ctohexstr(unsigned char hex, char *outbuf) {
-    unsigned char upper_half = hex >> 4;
-    unsigned char lower_half = hex & 0x0F;
-    if (upper_half <= 9) {
-        outbuf[0] = upper_half + '0';
-    } else {
-        outbuf[0] = upper_half + 'A' - 0x0a;
-    }
-    if (lower_half <= 9) {
-        outbuf[1] = lower_half + '0';
-    } else {
-        outbuf[1] = lower_half + 'A' - 0x0a;
-    }
+  unsigned char upper_half = hex >> 4;
+  unsigned char lower_half = hex & 0x0F;
+  if (upper_half <= 9) {
+    outbuf[0] = upper_half + '0';
+  } else {
+    outbuf[0] = upper_half + 'A' - 0x0a;
+  }
+  if (lower_half <= 9) {
+    outbuf[1] = lower_half + '0';
+  } else {
+    outbuf[1] = lower_half + 'A' - 0x0a;
+  }
 }
