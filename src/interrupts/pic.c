67dff543827a12052e3e157b969af619ff678196
Nathan <nathan.leakeas@gmail.com>
2024-03-11 08:23:09 -0500

    functions to interface with PIC

---
+++ src/interrupts/pic.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+
+#include "pic.h"
+#include "port.h"
+#include "intutils.h"
+
+void pic_send_eoi(uint8_t irq) {
+    if (irq >= 8) {
+        outb(PIC2_COMMAND, 0x20);
+    }
+    outb(PIC1_COMMAND, 0x20);
+}
+
+void pic_remap (int32_t off1, int32_t off2) {
+    uint32_t mask = disable();
+    uint8_t a1 = inb(PIC1_DATA);
+    uint8_t a2 = inb(PIC2_DATA);
+
+    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
+	outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+	outb(PIC1_DATA, off1);                 // ICW2: Master PIC vector offset
+	outb(PIC2_DATA, off2);                 // ICW2: Slave PIC vector offset
+	outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
+ 
+	outb(PIC1_DATA, ICW4_8086);               // ICW4: have the PICs use 8086 mode (and not 8080 mode)
+	outb(PIC2_DATA, ICW4_8086);
+ 
+	outb(PIC1_DATA, a1);   // restore saved masks.
+	outb(PIC2_DATA, a2);
+
+    restore(mask);
+
+}
